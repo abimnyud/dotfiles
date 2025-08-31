44ec35197f85371df29494909229c5a2e14e890a
Abimanyu <abimanyu.damarjati@gmail.com>
2025-09-01 00:08:00 +0700

    fix: tresitter error

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -17,3 +17,10 @@ return {
         "lua",
       },
       query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
